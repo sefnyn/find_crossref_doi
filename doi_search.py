712c6c0e69f4d7df5191554737a6bd52a671f299
sefnyn <Sefnyn@gmail.com>
2025-08-23 06:46:58 +0100

    alpha version of doi_search created

--- doi_search.py
+++ doi_search.py
@@ -24,18 +24,55 @@ import csv, sys
 # My modules
 import find_doi
 
+count = 0
+misses = 0
+hits = 0
+csv_misses = 'misses.csv'
+csv_hits   = 'hits.csv'
+
 def read_research_outputs(filename):
+    fh1 = open(csv_misses, 'w')
+    writer1 = csv.writer(fh1)
+    fh2 = open(csv_hits, 'w')
+    writer2   = csv.writer(fh2)
+    global count, misses, hits
     with open(filename, newline='') as f:
         reader = csv.reader(f, dialect='excel', delimiter=',')
         try:
             for row in reader:
-                if row[1] == 'Title' and row[2] == 'Journal':
+                oid     = row[0]
+                title   = row[1]
+                journal = row[2]
+                if title == 'Title' and journal == 'Journal':
                     print('Skipping field names...')
                 else:
                     print(row)
+                    count += 1
+                    result = find_doi.find(title, journal)
+                    if result == None:
+                        misses += 1
+                        writer1.writerow(row)
+
+                    else:
+                        hits += 1
+                        doi = result['doi']
+                        writer2.writerow([oid, title, journal, doi])
+            print('Processed ' + str(count) + ' journal articles')
+            print('Found ' + str(hits) + ' DOIs')
+            print(str(misses) + ' journal articles without a DOI')
         except csv.Error as e:
             sys.exit(f'file {filename}, line {reader.line_num}: {e}')
 
+
+def process_research_output(oid, ti, jo):
+    global count, misses, hits
+    result = find_doi.find(ti, jo)
+    if result == None:
+        misses += 1
+        print('Write to misses.csv')
+    else:
+        hits += 1
+        print('Write to hits.csv')
 
 
 def main():
